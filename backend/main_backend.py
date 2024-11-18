2f46dff985f3d6237879cfec02a2c2eaf79da800
gguevarab <g.guevarab@uniandes.edu.co>
2024-11-18 16:32:19 -0500

    fixed initial content folder

--- backend/main_backend.py
+++ backend/main_backend.py
@@ -1,16 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
-from dotenv import load_dotenv
 import os
-import openai
 
 from routes.books import books_bp
 from routes.material import material_bp
 from routes.generations import generations_bp
 
-# Load the access key
-load_dotenv()
-openai.api_key = os.environ.get('OPENAI_API_KEY')
 
 app = Flask(__name__)
 CORS(app)
@@ -21,8 +16,8 @@ app.register_blueprint(material_bp)
 app.register_blueprint(generations_bp)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    
     # If generations folder doesn't exist, create one
     if not os.path.exists('content'):
         os.makedirs('content')
+        
+    app.run(debug=True)
